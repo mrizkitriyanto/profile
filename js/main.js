023aa0ed95efc44ffa412cc2b975b8b3895ddbbc
mrizkitriyanto <muhamadrizkitriyanto41@gmail.com>
2021-01-14 00:53:33 +0700

    ganti konsep dan sudah sampai footer, kurang perbaikan minor

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 //SCROLL SECTION
-const sections = document.querySelectorAll('selection[id]')
+const sections = document.querySelectorAll('section[id]')
 
 window.addEventListener('scroll', scrollActive)
 
@@ -38,9 +38,9 @@ function scrollActive(){
     sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop -50;
-        sectionID = current.getAttribute('id')
+        sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionGeight){
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
         }else{
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
@@ -49,7 +49,35 @@ function scrollActive(){
 }
 
 /* SCROLL REVEAL ANIMATION */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
 /* SCROLL HOME */
+sr.reveal('.home__title', {})
+sr.reveal('.home__scroll', {delay: 200})
+sr.reveal('.home__img', {origin:'right', delay: 400})
+
 /* SCROLL ABOUT */
+sr.reveal('.about__img', {delay: 500})
+sr.reveal('.about__subtitle', {delay: 300})
+sr.reveal('.about__profession', {delay: 400})
+sr.reveal('.about__text', {delay: 500})
+sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+
 /* SCROLL SKILLS */
-/* SCROLL WORK */
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__name', { distance: '20px', delay: 50, interval: 100})
+sr.reveal('.skills__img', {delay: 400})
+
+/* SCROLL INTEREST */
+sr.reveal('.interest__img', {interval: 200})
+
+/*SCROLL CONTACT*/
+sr.reveal('.contact__subtitle', {})
+sr.reveal('.contact__text', {interval: 200})
+sr.reveal('.contact__input', {delay: 400})
+sr.reveal('.contact__button', {delay: 600})
